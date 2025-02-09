7048a2323267e441770d047908e939b5fe2e7697
Spencer <94135891+spencer005@users.noreply.github.com>
2025-02-09 09:38:55 -0500

    update

--- build.sh
+++ build.sh
@@ -2,8 +2,26 @@
 set -ouex pipefail
 
 dnf5() { command dnf5 -y "$@"; }
-dnf() { command dnf -y "$@"; }
 
+# https://github.com/ublue-os/bluefin/blob/main/build_files/shared/build-base.sh
+echo "::group:: Remove CLI Wrap"
+
+# there is no 'rpm-ostree cliwrap uninstall-from-root', but this is close enough. See:
+# https://github.com/coreos/rpm-ostree/blob/6d2548ddb2bfa8f4e9bafe5c6e717cf9531d8001/rust/src/cliwrap.rs#L25-L32
+if [ -d /usr/libexec/rpm-ostree/wrapped ]; then
+    # binaries which could be created if they did not exist thus may not be in wrapped dir
+    rm -f \
+        /usr/bin/yum \
+        /usr/bin/dnf \
+        /usr/bin/kernel-install
+    # binaries which were wrapped
+    mv -f /usr/libexec/rpm-ostree/wrapped/* /usr/bin
+    rm -fr /usr/libexec/rpm-ostree
+fi
+
+echo "::endgroup::"
+
+dnf5 copr enable ublue/os-akmods
 dnf5 copr enable bieszczaders/kernel-cachyos
 dnf5 copr enable bieszczaders/kernel-cachyos-lto
 dnf5 copr enable bieszczaders/kernel-cachyos-addons
@@ -37,6 +55,7 @@ remove=(
     "sudo"
     "vlc-libs"
     "vim-minimal"
+    "clang"
 )
 
 dnf5 remove "${remove[@]}"
@@ -54,9 +73,9 @@ kernel=(
 
 rpm --erase --nodeps -- "${kernel[@]}"
 
-# use dnf to make use of replace-kernel from rpm-ostree
-dnf install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
+dnf5 install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
 
+dnf5 copr disable ublue/os-akmods
 dnf5 copr disable bieszczaders/kernel-cachyos
 dnf5 copr disable bieszczaders/kernel-cachyos-addons
 dnf5 copr disable bieszczaders/kernel-cachyos-lto
