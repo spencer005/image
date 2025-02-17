ef437d8a571bd74244c5ad391ffd14f5f551442c
Spencer <94135891+spencer005@users.noreply.github.com>
2025-02-17 00:01:35 -0500

    revert

--- build.sh
+++ build.sh
@@ -3,22 +3,6 @@ set -ouex pipefail
 
 dnf5() { command dnf5 -y "$@"; }
 
-# https://github.com/ublue-os/bluefin/blob/main/build_files/shared/build-base.sh
-echo "::group:: Remove CLI Wrap"
-
-# there is no 'rpm-ostree cliwrap uninstall-from-root', but this is close enough. See:
-# https://github.com/coreos/rpm-ostree/blob/6d2548ddb2bfa8f4e9bafe5c6e717cf9531d8001/rust/src/cliwrap.rs#L25-L32
-if [ -d /usr/libexec/rpm-ostree/wrapped ]; then
-    # binaries which could be created if they did not exist thus may not be in wrapped dir
-    rm -f \
-        /usr/bin/yum \
-        /usr/bin/dnf \
-        /usr/bin/kernel-install
-    # binaries which were wrapped
-    mv -f /usr/libexec/rpm-ostree/wrapped/* /usr/bin
-    rm -fr /usr/libexec/rpm-ostree
-fi
-
 echo "::endgroup::"
 
 dnf5 copr enable ublue-os/akmods
@@ -73,7 +57,7 @@ kernel=(
 
 rpm --erase --nodeps -- "${kernel[@]}"
 
-dnf5 install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
+dnf install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
 
 dnf5 copr disable ublue-os/akmods
 dnf5 copr disable bieszczaders/kernel-cachyos
